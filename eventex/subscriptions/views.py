f30d106e087938ebb0366cd43aa8ce02222851fc
Renan Moura <rvmoura.96@gmail.com>
2018-06-24 13:12:41 -0300

    Habilita inscrição

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,6 +1,6 @@
 from django.contrib import messages
 from django.core import mail
-from django.http import HttpResponseRedirect, HttpResponse
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.template.loader import render_to_string
 from .forms import SubscriptionForm
