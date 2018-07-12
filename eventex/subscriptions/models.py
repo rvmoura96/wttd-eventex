f835076acd8243b0e819c19b6a8ccb351bdf8a2a
Renan Moura <rvmoura.96@gmail.com>
2018-07-12 20:21:04 -0300

    Adicionando controle de pagamentos de  inscrições

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -2,12 +2,12 @@ from django.db import models
 
 
 class Subscription(models.Model):
-    name = models.CharField(max_length=100, verbose_name='nome')
-    cpf = models.CharField(max_length=11, verbose_name='CPF')
-    email = models.EmailField(verbose_name='e-mail')
-    phone = models.CharField(max_length=20, verbose_name='telefone')
-    created_at = models.DateTimeField(auto_now_add=True,
-                                      verbose_name='Criado em')
+    name = models.CharField('nome', max_length=100)
+    cpf = models.CharField('CPF', max_length=11)
+    email = models.EmailField('e-mail')
+    phone = models.CharField('telefone', max_length=20)
+    created_at = models.DateTimeField('Criado em', auto_now_add=True)
+    paid = models.BooleanField('pago', default=False)
 
     class Meta:
         verbose_name_plural = 'inscrições'
