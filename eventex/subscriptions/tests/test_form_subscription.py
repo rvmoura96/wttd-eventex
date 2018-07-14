526768c2a06968dc40795d1fa5737114714fc9ca
Renan Moura <rvmoura.96@gmail.com>
2018-07-13 21:07:10 -0300

    Padronizando o nome para Capital Letters.

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -20,6 +20,13 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(cpf='1234')
         self.assertFormErrorCode(form, 'cpf', 'length')
 
+    def test_name_must_be_capitalized(self):
+        """Name must be capitalized.
+        if name==RENAN moura should be => Renan Moura.
+        """
+        form = self.make_validated_form(name='RENAN moura')
+        self.assertEqual('Renan Moura', form.cleaned_data['name'])
+
     def assertFormErrorCode(self, form, field, code):
         errors = form.errors.as_data()
         erros_list = errors[field]
