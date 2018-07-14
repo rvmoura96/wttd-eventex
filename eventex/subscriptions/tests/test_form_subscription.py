fb7d041a5c4c08880ced8754785b3df44e57d13b
Renan Moura <rvmoura.96@gmail.com>
2018-07-13 21:41:09 -0300

    Validando campos para contato, onde é necessário o informar ao menos um dos meios, seja e-mail ou telefone.

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -27,6 +27,21 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(name='RENAN moura')
         self.assertEqual('Renan Moura', form.cleaned_data['name'])
 
+    def test_email_is_optional(self):
+        """Email is an optional field."""
+        form = self.make_validated_form(email='')
+        self.assertFalse(form.errors)
+
+    def test_phone_is_optional(self):
+        """Phone is an optional field."""
+        form = self.make_validated_form(phone='')
+        self.assertFalse(form.errors)
+
+    def test_must_inform_email_or_phone(self):
+        """Email and Phone are optional, but one must be informed."""
+        form = self.make_validated_form(phone='', email='')
+        self.assertListEqual(['__all__'], list(form.errors))
+
     def assertFormErrorCode(self, form, field, code):
         errors = form.errors.as_data()
         erros_list = errors[field]
@@ -40,7 +55,7 @@ class SubscriptionFormTest(TestCase):
         return self.assertEqual([msg], errors_list)
 
     def make_validated_form(self, **kwargs):
-        valid = dict(name='Renan Moura', cpf='1234678901',
+        valid = dict(name='Renan Moura', cpf='12345678901',
                     email='rvmoura.96@gmail.com', phone='11-933339999')
         data = dict(valid, **kwargs)
         form = SubscriptionForm(data)
--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -85,3 +85,10 @@ class SubscribePostInvalid(TestCase):
     def test_dont_save_subscription(self):
         """Subscription invalid POST can't save on database."""
         self.assertFalse(Subscription.objects.exists())
+
+class TemplateRegressionTest(TestCase):
+    def test_template_has_non_field_errors(self):
+        invalid_data = dict(name='Renan Moura', cpf='12345678901')
+        response = self.client.post(r('subscriptions:new'), invalid_data)
+
+        self.assertContains(response, '<ul class="errorlist nonfield">')
